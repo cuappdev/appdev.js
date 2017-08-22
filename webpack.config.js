174d8b8562d2e641cb0c57744232137c98567ee7
Jma353 <jma353@cornell.edu>
2017-08-22 16:51:04 -0400

    Remove package-lock.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
-/* global __dirname, require, module*/
-
+const fs = require('fs');
 const webpack = require('webpack');
 const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 const path = require('path');
@@ -7,7 +6,8 @@ const env = require('yargs').argv.env; // use --env with webpack 2
 
 let libraryName = 'Library';
 
-let plugins = [], outputFile;
+let plugins = [];
+let outputFile = null;
 
 if (env === 'build') {
   plugins.push(new UglifyJsPlugin({ minimize: true }));
@@ -16,27 +16,40 @@ if (env === 'build') {
   outputFile = libraryName + '.js';
 }
 
+const nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter(function (x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function (mod) {
+    nodeModules[mod] = 'commonjs ' + mod;
+  });
+
 const config = {
-  entry: __dirname + '/src/index.js',
+  entry: ['babel-polyfill', path.join(__dirname, '/src/index.js')],
   devtool: 'source-map',
   output: {
-    path: __dirname + '/lib',
+    path: path.join(__dirname, '/lib'),
     filename: outputFile,
     library: libraryName,
     libraryTarget: 'umd',
     umdNamedDefine: true
   },
+  externals: nodeModules,
   module: {
     rules: [
       {
-        test: /(\.jsx|\.js)$/,
+        test: /\.js?$/,
         loader: 'babel-loader',
-        exclude: /(node_modules|bower_components)/
+        exclude: /(node_modules)/,
+        query: {
+          presets: ['es2015', 'stage-2']
+        }
       },
       {
         test: /(\.jsx|\.js)$/,
         loader: 'eslint-loader',
-        exclude: /node_modules/
+        exclude: /(node_modules)/
       }
     ]
   },
