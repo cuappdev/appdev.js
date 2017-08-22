54e38f17793fac5f6857cc62a8800588dba6403d
Jma353 <jma353@cornell.edu>
2017-08-22 15:50:27 -0400

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+// @flow
+export type RequestType = 'POST' | 'GET' | 'DELETE';
+
+const REQUEST_TYPES = {
+  POST: 'POST',
+  GET: 'GET',
+  DELETE: 'DELETE'
+};
+
+export default {
+  REQUEST_TYPES
+};
---
+++ src/index.js
@@ -0,0 +1,9 @@
+// @flow
+import AppDevRouter from './AppDevRouter';
+
+import appDevUtils from './appDevUtils';
+
+export default {
+  AppDevRouter,
+  appDevUtils
+};
