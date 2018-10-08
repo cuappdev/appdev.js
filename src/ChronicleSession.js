88e508721b3ff57c6da11f11721ca66834a8e860
aliciaxw <alicia.xinran.wang@gmail.com>
2018-10-08 11:50:06 -0400

    Fix cacheSize = 1 logging issue

--- src/ChronicleSession.js
+++ src/ChronicleSession.js
@@ -10,7 +10,7 @@ class ChronicleSession {
   app: string;
   cacheSize: number;
   logMap: Map<string, Object[]>;
-  s3: S3;
+  s3: S3; 
 
   constructor(
     accessKey: string, 
@@ -34,9 +34,9 @@ class ChronicleSession {
     const schema = new ParquetSchema(eventType);
     const writer = await ParquetWriter.openFile(schema, `${PATH}/${filename}`);
 
-    const logs = this.logMap.get(eventName);
+    let logs = this.logMap.get(eventName);
     if (logs == undefined) { // sanity check
-      return;
+      logs = [];
     }
 
     logs.forEach(log => writer.appendRow(log));
@@ -52,12 +52,11 @@ class ChronicleSession {
   }
 
   async log(eventName: string, eventType: ParquetSchema, event: Object) {
-    const logs = this.logMap.get(eventName);
+    let logs = this.logMap.get(eventName);
     if (logs == undefined) {
       this.logMap.set(eventName, [event]);
-      return;
-    } 
-
+      logs = [];
+    }
     logs.push(event);
     this.logMap.set(eventName, logs);
     if (logs.length >= this.cacheSize) {
