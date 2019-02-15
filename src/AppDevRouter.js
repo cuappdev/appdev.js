043c0606a983af095aebd913f3c5320135fc829f
Kevin Chan <kevinchan159@gmail.com>
2019-02-14 19:58:42 -0500

    Support multiple request types

--- src/AppDevRouter.js
+++ src/AppDevRouter.js
@@ -1,7 +1,11 @@
 // @flow
-
+import {
+  NextFunction,
+  Request,
+  Response,
+  Router,
+} from 'express';
 import AppDevUtilities from './AppDevUtilities';
-import { Router, Request, Response, NextFunction } from 'express';
 
 /**
  * RequestType - the HTTP methods supported by AppDevRouter
@@ -10,14 +14,15 @@ export type RequestType = 'GET' | 'POST' | 'DELETE';
 
 /**
  * AppDevResponse - the response from an HTTP request
- * 
+ *
  * Wraps a `success` field around the response data
  */
 class AppDevResponse<T> {
   success: boolean;
+
   data: T;
 
-  constructor (success: boolean, data: T) {
+  constructor(success: boolean, data: T) {
     this.success = success;
     this.data = data;
   }
@@ -26,20 +31,21 @@ class AppDevResponse<T> {
 /**
  * AppDevRouter - cleanly create an Express Router object using inheritance
  *
- * Subclasses can simply specify the HTTP method, the path, and a response 
- * hook to compute response data. This pattern is cleaner than raw Express 
+ * Subclasses can simply specify the HTTP method, the path, and a response
+ * hook to compute response data. This pattern is cleaner than raw Express
  * Router initialization with callbacks.
  */
 class AppDevRouter<T> {
   router: Router;
-  requestType: RequestType;
+
+  requestTypes: Array<RequestType>;
 
   /**
    * Subclasses must call this constructor and pass in the HTTP method
    */
-  constructor(type: RequestType) {
+  constructor(types: RequestType) {
     this.router = new Router();
-    this.requestType = type;
+    this.requestTypes = types;
 
     // Initialize this router
     this.init();
@@ -56,19 +62,21 @@ class AppDevRouter<T> {
     AppDevUtilities.tryCheckAppDevURL(path);
 
     // Attach content to router
-    switch (this.requestType) {
-    case 'GET':
-      this.router.get(path, this.response());
-      break;
-    case 'POST':
-      this.router.post(path, this.response());
-      break;
-    case 'DELETE':
-      this.router.delete(path, this.response());
-      break;
-    default:
-      throw new Error("HTTP method not specified!");
-    }
+    this.requestTypes.forEach((reqType) => {
+      switch (reqType) {
+        case 'GET':
+          this.router.get(path, this.response());
+          break;
+        case 'POST':
+          this.router.post(path, this.response());
+          break;
+        case 'DELETE':
+          this.router.delete(path, this.response());
+          break;
+        default:
+          throw new Error('HTTP method not supported!');
+      }
+    });
   }
 
   /**
@@ -83,13 +91,13 @@ class AppDevRouter<T> {
    * Subclasses must override this response hook to generate response data
    * for the given request.
    */
-  async content (req: Request): Promise<T> {
+  async content(req: Request): Promise<T> {
     throw new Error(1);
   }
 
   /**
    * Create a wrapper around the response hook to pass to the Express
-   * Router. 
+   * Router.
    */
   response() {
     return async (req: Request, res: Response, next: NextFunction) => {
@@ -100,14 +108,12 @@ class AppDevRouter<T> {
         if (e.message === 1) {
           throw new Error('You must implement content()!');
         } else {
-          res.json(new AppDevResponse(false, {errors: [e.message]}));
+          res.json(new AppDevResponse(false, { errors: [e.message] }));
         }
       }
       next();
-    }
-  } 
-
-  
+    };
+  }
 }
 
 export default AppDevRouter;
